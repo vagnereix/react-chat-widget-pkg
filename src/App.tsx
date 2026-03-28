8a5d3a1053341206e4e0c5270a731feb4e220421
Vagner Reis <vagnereix.dev@gmail.com>
2026-03-28 15:16:47 -0300

    fix: remove placeholder text from App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { ChatWidget } from "@/components/chat-widget";
 function App() {
   return (
     <main className="bg-chat-widget-background">
-      oi
       <ChatWidget className="chat-color-palette" isInMaintenanceMode={false} />
     </main>
   );
